5d1bfafd5336148a3a839b79c3e73fe286515200
Juan Carlos <jbarcinilla@outlook.com>
2025-03-06 22:49:39 -0500

    se configuro sprint security se agrega page de error

---
+++ backend/src/main/java/com/example/demo/controlles/CustomErrorController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controlles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+
+    public String getErrorPath() {
+        return "/error";
+    }
+}
+
